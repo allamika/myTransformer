3a65a6fea087c7598367a39a3cbe5590f4634142
MAllanos <mikael.allanos432@gmail.com>
2024-05-16 01:03:33 +0200

    create DataLoader

--- Data.py
+++ Data.py
@@ -1,6 +1,10 @@
 from Tokenizer import BasicTextTokenizer
 
+from tqdm import tqdm
+
 import torch
+from torch.utils.data import DataLoader
+
 
 class Data():
     def __init__(self, ration = 0.9, bite_pair_encoding=0):
@@ -11,10 +15,15 @@ class Data():
         self.tokenizer = BasicTextTokenizer(text, bite_pair_encoding)
         
         print("---Data Encoding---")
-        data = self.tokenizer.encode(text)
-        n = int(len(data)*ration)
-        self.train_data = data[:n]
-        self.test_data = data[n:]
+        self.data = self.tokenizer.encode(text)
+        n = int(len(self.data)*ration)
+        self.train_data = self.data[:n]
+        self.test_data = self.data[n:]
+
+    def getDataLoader(self, block_size,  ratio = 0.9):
+        print("---Creating DataLoader---")
+        data = [(self.data[i:i+block_size], self.data[i+1:i+block_size+1]) for i in tqdm(range(len(self.data)-block_size-1))]
+        return DataLoader(data)
     
     def get_batch(self, data, batch_size, block_size):
         if data == "train": data = self.train_data
@@ -28,6 +37,9 @@ class Data():
 
 if __name__ == "__main__":
     data = Data()
+    dataLoader = data.getDataLoader(8)
+    features, labels = next(iter(dataLoader))
+    print(f"Features:\n {features}, Labels\n: {labels}")
     tokenizer = data.tokenizer
     
     print(f"First 500 token of training data:\n\n{tokenizer.decode(data.train_data[:500])}")
